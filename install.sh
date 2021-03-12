3ee3ede2ddbb57e92177e97687793bd23874bc7f
Phil Carney <philipccarney@gmail.com>
2021-03-12 12:31:57 +0000

    Before refreshing line endings

--- install.sh
+++ install.sh
@@ -5,15 +5,26 @@ rm -rf $CLONE_DIR && mkdir -p "$CLONE_DIR"
 if [[ $NAME == 'Ubuntu' ]]
 then
     echo "Installing pre-reqs..." && \
-    apt-get update && apt-get install -y git curl software-properties-common && \
+    apt-get update > /dev/null 2> /dev/null && \
+    apt-get install -y git curl software-properties-common > /dev/null 2> /dev/null && \
     echo "Installing ansible repository..." && \
-    apt-add-repository --yes --update ppa:ansible/ansible && \
+    apt-add-repository --yes --update ppa:ansible/ansible > /dev/null 2> /dev/null && \
     echo "Installing ansible..." && \
-    apt-get install -y ansible
+    apt-get install -y ansible > /dev/null 2> /dev/null
 fi
 
 echo "Cloning playbook to $CLONE_DIR" && \
 git clone https://github.com/phlcrny/ansible-setup.git $CLONE_DIR && \
-echo "Running playbook" && \
+chmod 0755 $CLONE_DIR -r && \
+
+# Check for 3 params
+
+# Check for 2 params
+
+# Check for 1 param
+
+# else
+
+echo "Running playbook with defaults" && \
 ansible-playbook "$CLONE_DIR/playbook.yml" && \
 crontab -l
