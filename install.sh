f9e8b29c882e5ed48c3ca9b128662c2831a3c8db
Phil Carney <philipccarney@gmail.com>
2021-03-30 21:16:43 +0100

    Add support for custom variables

--- install.sh
+++ install.sh
@@ -2,6 +2,24 @@
 CLONE_DIR=/tmp/ansible-setup/
 NAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d [:punct:])
 rm -rf $CLONE_DIR && mkdir -p "$CLONE_DIR"
+# Get the interactive prompts out of the way - no interruptions
+echo -n "Run playbook with custom vars? (yes/no): "
+read choice
+if [ "$choice" == "y*" ]; then
+    echo -n "Enter username (default: phil): "
+    read username
+    if [ "$choice" == "" ]; then
+        username="phil"
+    fi
+    echo "User: $username"
+    echo -n "Enter dotfiles repo (default: https://github.com/JohnDoe/dotfiles.git): "
+    read dotfiles
+    if [ "$dotfiles" == "" ]; then
+        dotfiles="https://github.com/JohnDoe/dotfiles.git"
+    fi
+    echo "dotfiles: $dotfiles"
+fi
+
 if [[ $NAME == 'Ubuntu' ]]
 then
     echo "Installing pre-reqs..." && \
@@ -14,9 +32,16 @@ then
 fi
 
 echo "Cloning playbook to $CLONE_DIR" && \
-git clone https://github.com/phlcrny/ansible-setup.git $CLONE_DIR && \
+git clone https://github.com/phlcrny/ansible-setup.git $CLONE_DIR
 
-echo "Running playbook with defaults" && \
-ansible-playbook "$CLONE_DIR/playbook.yml" && \
-echo "Reviewing creating cron jobs" && \
-crontab -l
+if [ "$choice" == "y*" ]; then
+    echo "Running playbook with custom variables: '$username' and '$dotfiles'" && \
+    ansible-playbook "$CLONE_DIR/playbook.yml" -e "user=$username dotfiles_src=$dotfiles" && \
+    echo "Reviewing creating cron jobs" && \
+    crontab -l
+else
+    echo "Running playbook with defaults" && \
+    ansible-playbook "$CLONE_DIR/playbook.yml" && \
+    echo "Reviewing creating cron jobs" && \
+    crontab -l
+fi
